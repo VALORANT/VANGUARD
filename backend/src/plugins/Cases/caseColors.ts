3ad5d7763343a0fd047b3fa181717c8cedb64450
Lily Bergonzat <lilybergonzat@gmail.com>
2024-01-16 05:33:10 +0100

    Updated case colours

--- backend/src/plugins/Cases/caseColors.ts
+++ backend/src/plugins/Cases/caseColors.ts
@@ -1,13 +1,13 @@
 import { CaseTypes } from "../../data/CaseTypes";
 
 export const caseColors: Record<CaseTypes, number> = {
-  [CaseTypes.Ban]: 0xcb4314,
-  [CaseTypes.Unban]: 0x9b59b6,
-  [CaseTypes.Note]: 0x3498db,
-  [CaseTypes.Warn]: 0xdae622,
-  [CaseTypes.Mute]: 0xe6b122,
-  [CaseTypes.Unmute]: 0xa175b3,
-  [CaseTypes.Kick]: 0xe67e22,
-  [CaseTypes.Deleted]: 0x000000,
-  [CaseTypes.Softban]: 0xe67e22,
+  [CaseTypes.Ban]: 0xfc3e5c,
+  [CaseTypes.Unban]: 0x4bc271,
+  [CaseTypes.Note]: 0xb2b7d2,
+  [CaseTypes.Warn]: 0xf4c13c,
+  [CaseTypes.Mute]: 0xf3693e,
+  [CaseTypes.Unmute]: 0x4bc271,
+  [CaseTypes.Kick]: 0xfc3e5c,
+  [CaseTypes.Deleted]: 0xb2b7d2,
+  [CaseTypes.Softban]: 0xfc3e5c,
 };
